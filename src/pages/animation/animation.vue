2b8a36cfc17e823258205752abf33b45abb6130d
liuwenjun <15838995173@163.com>
2019-05-21 17:07:19 +0800

    新增动画

---
+++ src/pages/animation/animation.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="animation">
+        <Button type="primary" @click="()=>show=!show">toggle</Button>
+        <transition name="fade">
+            <p v-if="show">
+                hello
+            </p>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"animation",
+        data(){
+            return {
+                show:true
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style lang="less">
+    .animation{
+        .fade-enter-active, .fade-leave-active {
+            transition: opacity .5s;
+        }
+        .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+            opacity: 0;
+        }
+    }
+</style>
+
---
+++ src/pages/animation/route.js
@@ -0,0 +1,6 @@
+const animation =()=>import('./animation')
+export default [{
+  path:'/animation',
+  name:'animation',
+  component:animation
+}]
